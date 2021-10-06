a27642786a838e64a60c5e8e71c7d0ad8bc7dd75
PhantomApps <kla.picaku@stud.uniroma3.it>
2021-10-06 21:50:55 +0200

    added attack command, finally

--- my first game third attempt/Assets/CharacterAttack.cs
+++ my first game third attempt/Assets/CharacterAttack.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class CharacterAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayer;
+    public bool isAttacking = false;
+    bool attacker = false;
+    public void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        if (isAttacking)
+        {
+            attacker = true;
+        }
+        else
+        {
+            attacker = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        Attack(attacker);
+    }
+    void Attack(bool attack)
+    {
+        if (attack)
+        {
+            animator.SetBool("IsAttacking", true);
+
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                Debug.Log("We hit" + enemy.name);
+            }
+        }
+    }
+    public void startAttack()
+    {
+        attacker = true;
+        animator.SetBool("IsAttacking", true);
+    }
+    public void stopAttacking()
+    {
+        attacker = false;
+        animator.SetBool("IsAttacking",false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
